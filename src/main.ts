59cd75a62650eb9ec606dc94f17f88890647fa26
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-08-26 16:51:17 -0700

    port fix on main.ts => SERVER_PORT

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService)
-  const port = configService.get('PORT')
+  const port = configService.get('SERVER_PORT')
 
   await app.listen(port);
 
